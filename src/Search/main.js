f2a2d9cb302c351c29091164672f3fabf9af81c8
Abhimanyusars <18bcs003@smvdu.ac.in>
2022-08-09 01:55:21 +0530

    new

--- src/Search/main.js
+++
@@ -1,7 +0,0 @@
-fetch('data/index.json').then(response => {
-    return response.json();
-  }).then(json => {
-    index = elasticlunr.Index.load(json);
-  });
-  const matches = index.search(‘foo’);
-  console.log(hey);
